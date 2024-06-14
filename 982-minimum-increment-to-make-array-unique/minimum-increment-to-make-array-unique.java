ca4c1c243564ddb7528cb01825babe91982612f3
UTSAV SINGHAL <utsavsinghal26@gmail.com>
2024-06-14 15:27:02 +0530

    Time: 37 ms (72.13%) | Memory: 60.2 MB (44.92%) - LeetSync

---
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+
+        int numTracker = 0;
+        int minIncrement = 0;
+
+        for (int num : nums) {
+            numTracker = Math.max(numTracker, num);
+            minIncrement += numTracker - num;
+            numTracker += 1;
+        }
+
+        return minIncrement;
+    }
+}
