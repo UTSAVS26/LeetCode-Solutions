2cbf0fb69b8934451544f882565542529258b63f
UTSAV SINGHAL <utsavsinghal26@gmail.com>
2024-07-07 13:34:11 +0530

    Time: 44 ms (5.97%) | Memory: 16.5 MB (32.78%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        totalBottles = numBottles
+
+        while numBottles >= numExchange:
+            totalBottles += numBottles // numExchange
+            numBottles = numBottles // numExchange + numBottles % numExchange
+
+        return totalBottles
